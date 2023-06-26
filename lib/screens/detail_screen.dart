50723f2fa944601f15174e09364c588e36edd14d
Alice2333333333 <alicelau0080@gmail.com>
2023-06-26 21:11:27 +0200

    update detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -26,11 +26,59 @@ class DetailScreen extends StatelessWidget {
             ),
             child: CachedImage(imageUrl: image.url),
           ),
-          Text("Description:"),
-          Text("Location:"),
-          Text("Date:"),
-          Text("Time:"),
-          Text("Local path:"),
+          const InfoText(
+            title: "Description:",
+            text: "abc",
+          ),
+          const InfoText(
+            title: "Location:",
+            text: "abc",
+          ),
+          const InfoText(
+            title: "Date:",
+            text: "abc",
+          ),
+          const InfoText(
+            title: "Time:",
+            text: "abc",
+          ),
+          const InfoText(
+            title: "Local path:",
+            text: "abc",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InfoText extends StatelessWidget {
+  const InfoText({
+    super.key,
+    required this.title,
+    required this.text,
+  });
+
+  final String title;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 25.0,
+        vertical: 5.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(text),
         ],
       ),
     );
