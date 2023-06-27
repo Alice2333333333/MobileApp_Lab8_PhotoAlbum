f0d124cc69d671a429b66bcf21be949e55c7bca5
Alice2333333333 <alicelau0080@gmail.com>
2023-06-27 11:23:56 +0200

    update detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,9 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import 'package:photo_album/components/image_caller.dart';
 import 'package:photo_album/models/image_data.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({
     super.key,
     required this.image,
@@ -14,60 +16,13 @@ class DetailScreen extends StatelessWidget {
   final bool editMode;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Detail Info"),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 5.0,
-              vertical: 5.0,
-            ),
-            child: image.isUrl
-                ? CachedImage(imageUrl: image.path)
-                : PickedImage(imagePath: image.path),
-          ),
-          const InfoText(
-            title: "Description:",
-            text: "abc",
-          ),
-          const InfoText(
-            title: "Location:",
-            text: "abc",
-          ),
-          const InfoText(
-            title: "Date:",
-            text: "abc",
-          ),
-          const InfoText(
-            title: "Time:",
-            text: "abc",
-          ),
-          const InfoText(
-            title: "Local path:",
-            text: "abc",
-          ),
-        ],
-      ),
-    );
-  }
+  State<DetailScreen> createState() => _DetailScreenState();
 }
 
-class InfoText extends StatelessWidget {
-  const InfoText({
-    super.key,
-    required this.title,
-    required this.text,
-  });
+class _DetailScreenState extends State<DetailScreen> {
+  late bool editMode = widget.editMode;
 
-  final String title;
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget infoText(String title, String text, {bool editMode = false}) {
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 25.0,
@@ -82,8 +37,86 @@ class InfoText extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          Text(text),
+          editMode ? descriptionField(text) : Text(text),
         ],
+      ),
+    );
+  }
+
+  TextFormField descriptionField(String text) {
+    return TextFormField(
+      initialValue: text,
+      maxLines: 3,
+      autofocus: true,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        contentPadding: EdgeInsets.all(8.0),
+      ),
+      onChanged: (value) {
+        log(value);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Detail Info"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.only(
+          top: 2.5,
+          bottom: 75.0,
+        ),
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 5.0,
+              vertical: 2.5,
+            ),
+            child: widget.image.isUrl
+                ? CachedImage(imageUrl: widget.image.path)
+                : PickedImage(imagePath: widget.image.path),
+          ),
+          infoText(
+            "Description:",
+            "abc",
+            editMode: editMode,
+          ),
+          infoText(
+            "Location:",
+            "abc",
+          ),
+          infoText(
+            "Date:",
+            "abc",
+          ),
+          infoText(
+            "Time:",
+            "abc",
+          ),
+          infoText(
+            "Local path:",
+            "abcooooooooooooooooooooooooooo",
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          if (editMode) {
+            log("save");
+          }
+          setState(() {
+            editMode = !editMode;
+          });
+        },
+        icon: Icon(
+          editMode ? Icons.save : Icons.edit,
+        ),
+        label: Text(
+          editMode ? "Save" : "Edit",
+        ),
       ),
     );
   }
